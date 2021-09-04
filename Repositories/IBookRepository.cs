e3639e7c9e0a8e571cf104224cccebbc2a0b79c4
Admin_SYDANI <Admin_SYDANI@LAPTOP-4IGD9K2C>
2021-09-04 18:08:25 +0100

    Add project files.

---
+++ Repositories/IBookRepository.cs
@@ -0,0 +1,21 @@
+﻿using BooksAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int id);
+
+        Task<Book> Create(Book book);
+
+        Task Update(Book book);
+
+        Task Delete(int id);
+    }
+}
